408bc2fe426a1c18762eed3475d57a3b9aad4e62
Tiisetso Tjabane <t.tjabane@gmail.com>
2020-02-18 23:45:39 +0200

    removed the old files

--- TechTest/AnyCompany/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnyCompany
-{
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public double Amount { get; set; }
-        public double VAT { get; set; }
-    }
-}
